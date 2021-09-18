99b6701355fcf8bddcb7563427973534cb97f070
yash.garg <yash.garg@adpushup.com>
2021-09-18 12:35:23 +0530

    ap optimizer frontend

--- client/app/index.js
+++ client/app/index.js
@@ -1,23 +1,5 @@
 import React from "react";
-import { render } from "react-dom";
-
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-
+import ReactDOM from "react-dom";
 import App from "./components/App/App";
-import NotFound from "./components/App/NotFound";
 
-import Home from "./components/Home/Home";
-
-import "./styles/styles.scss";
-
-render(
-  <Router>
-    <App>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </App>
-  </Router>,
-  document.getElementById("app")
-);
+ReactDOM.render(<App />, document.getElementById("app"));
